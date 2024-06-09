4ac554def7d8d969862b6cd2767f1b079d0f8d29
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-06-09 10:29:06 +0000

    number and booleans

--- Chapter2/01.js
+++ Chapter2/01.js
@@ -1,2 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//string type
 var user = "aayush";
 console.log(user);
+//number type
+var userid = 34.56;
+console.log(userid.toPrecision());
+//boolean type
+var islogged = false;
+console.log(islogged.valueOf());
--- Chapter2/01.ts
+++ Chapter2/01.ts
@@ -1,3 +1,11 @@
+//string type
 let user:string="aayush";
 console.log(user)
-export {}
+//number type
+const userid:number=34.56;
+console.log(userid.toPrecision());
+//boolean type
+let islogged:boolean=false;
+console.log(islogged);
+
+export{}
