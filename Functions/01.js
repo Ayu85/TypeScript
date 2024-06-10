ef2ae7ab904d010ae295a87a3bc6c76016fdd91e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-06-10 09:17:56 +0000

    functions in ts

--- Functions/01.js
+++ Functions/01.js
@@ -1,7 +1,11 @@
 "use strict";
+// this is not possible
+// function add(num:number){
+//     console.log(num+2);
 Object.defineProperty(exports, "__esModule", { value: true });
+// }
+// add("aayush");
 function add(num) {
-    console.log(num + 2);
-    num.toUpperCase();
+    console.log(num + 5);
 }
-add(5);
+add(2.5);
--- Functions/01.ts
+++ Functions/01.ts
@@ -1,8 +1,13 @@
-function add(num){
-    console.log(num+2);
-    num.toUpperCase()
+// this is not possible
+// function add(num:number){
+//     console.log(num+2);
+  
+// }
+// add("aayush");
+
+// this is possible
+function add(num:number){
+    console.log(num+5)
 }
-add(5);
-
-
+add(2.5)
 export{}
