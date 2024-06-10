24dc56bb8f51c87108fefc85c8709ed3b3dba9fe
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-06-10 09:13:50 +0000

    functions in typescript

---
+++ Functions/01.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function add(num) {
+    console.log(num + 2);
+    num.toUpperCase();
+}
+add(5);
---
+++ Functions/01.ts
@@ -0,0 +1,8 @@
+function add(num){
+    console.log(num+2);
+    num.toUpperCase()
+}
+add(5);
+
+
+export{}
